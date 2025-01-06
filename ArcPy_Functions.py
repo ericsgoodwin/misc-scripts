5e37078a48dacac33d7106d0ad18c4cdd18c4f04
u1372868 <ericsgoodwin98@gmail.com>
2025-01-06 12:13:15 -0700

    Added "get_bbox" function to ArcPy_Functions.py

--- ArcPy_Functions.py
+++ ArcPy_Functions.py
@@ -2,6 +2,26 @@ import arcpy
 import pandas as pd
 
 
+def get_bbox(fc, feature_index=None):
+    """
+    Get the bounding box in a comma-delineated format of a polygon feature class, or a feature in a polygon fc.
+
+    Args:
+        fc: Polygon feature class
+        feature_index: the index value of the polygon fc. Used for looping through the fc.
+
+    Returns: List of values [minx, miny, maxx, maxy]
+
+    """
+    with arcpy.da.SearchCursor(fc, ["SHAPE@"]) as cursor:
+        for i, row in enumerate(cursor):
+            if feature_index:
+                if i == feature_index:
+                    extent = row[0].extent
+                    minx, maxx, miny, maxy = extent.XMin, extent.XMax, extent.YMin, extent.YMax
+                    return [minx, miny, maxx, maxy]
+
+
 def table_to_data_frame(in_table, input_fields=None, where_clause=None):
     """Function will convert an arcgis table into a pandas dataframe with an object ID index, and the selected
     input fields using an arcpy.da.SearchCursor.
